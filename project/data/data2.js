542057f733a664409606a85c68ee7ecf6ceda722
Florian Prucker <f.prucker@students.htl-leonding.ac.at>
2025-04-23 21:55:22 +0200

    update abend

---
+++ project/data/data2.js
@@ -0,0 +1,862 @@
+let data2 = [
+    {
+        cName: "Afghanistan",
+        cID: "AF",
+        continent: "Asien"
+    },
+    {
+        cName: "Ägypten",
+        cID: "EG",
+        continent: "Afrika"
+    },
+    {
+        cName: "Albanien",
+        cID: "AL",
+        continent: "Europa"
+    },
+    {
+        cName: "Algerien",
+        cID: "DZ",
+        continent: "Afrika"
+    },
+    {
+        cName: "Angola",
+        cID: "AO",
+        continent: "Afrika"
+    },
+    {
+        cName: "Äquatorialguinea",
+        cID: "GQ",
+        continent: "Afrika"
+    },
+    {
+        cName: "Argentinien",
+        cID: "AR",
+        continent: "Südamerika"
+    },
+    {
+        cName: "Armenien",
+        cID: "AM",
+        continent: "Asien"
+    },
+    {
+        cName: "Aserbaidschan",
+        cID: "AZ",
+        continent: "Asien"
+    },
+    {
+        cName: "Äthiopien",
+        cID: "ET",
+        continent: "Afrika"
+    },
+    {
+        cName: "Australien",
+        cID: "AU",
+        continent: "Ozeanien"
+    },
+    {
+        cName: "Bahamas",
+        cID: "BS",
+        continent: "Nordamerika"
+    },
+    {
+        cName: "Bahrain",
+        cID: "BH",
+        continent: "Asien"
+    },
+    {
+        cName: "Bangladesch",
+        cID: "BD",
+        continent: "Asien"
+    },
+    {
+        cName: "Belarus",
+        cID: "BY",
+        continent: "Europa"
+    },
+    {
+        cName: "Belgien",
+        cID: "BE",
+        continent: "Europa"
+    },
+    {
+        cName: "Belize",
+        cID: "BZ",
+        continent: "Nordamerika"
+    },
+    {
+        cName: "Benin",
+        cID: "BJ",
+        continent: "Afrika"
+    },
+    {
+        cName: "Bhutan",
+        cID: "BT",
+        continent: "Asien"
+    },
+    {
+        cName: "Bolivien",
+        cID: "BO",
+        continent: "Südamerika"
+    },
+    {
+        cName: "Bosnien und Herzegowina",
+        cID: "BA",
+        continent: "Europa"
+    },
+    {
+        cName: "Botswana",
+        cID: "BW",
+        continent: "Afrika"
+    },
+    {
+        cName: "Brasilien",
+        cID: "BR",
+        continent: "Südamerika"
+    },
+    {
+        cName: "Brunei",
+        cID: "BN",
+        continent: "Asien"
+    },
+    {
+        cName: "Bulgarien",
+        cID: "BG",
+        continent: "Europa"
+    },
+    {
+        cName: "Burkina Faso",
+        cID: "BF",
+        continent: "Afrika"
+    },
+    {
+        cName: "Myanmar",
+        cID: "BU",
+        continent: "Asien"
+    },
+    {
+        cName: "Burundi",
+        cID: "BI",
+        continent: "Afrika"
+    },
+    {
+        cName: "Chile",
+        cID: "CL",
+        continent: "Südamerika"
+    },
+    {
+        cName: "China",
+        cID: "CN",
+        continent: "Asien"
+    },
+    {
+        cName: "Costa Rica",
+        cID: "CR",
+        continent: "Nordamerika"
+    },
+    {
+        cName: "Dänemark",
+        cID: "DK",
+        continent: "Europa"
+    },
+    {
+        cName: "Deutschland",
+        cID: "DE",
+        continent: "Europa"
+    },
+    {
+        cName: "Dominikanische Republik",
+        cID: "DO",
+        continent: "Nordamerika"
+    },
+    {
+        cName: "Dschibuti",
+        cID: "DJ",
+        continent: "Afrika"
+    },
+    {
+        cName: "Ecuador",
+        cID: "EC",
+        continent: "Südamerika"
+    },
+    {
+        cName: "Elfenbeinküste",
+        cID: "CI",
+        continent: "Afrika"
+    },
+    {
+        cName: "El Salvador",
+        cID: "SV",
+        continent: "Nordamerika"
+    },
+    {
+        cName: "Eritrea",
+        cID: "ER",
+        continent: "Afrika"
+    },
+    {
+        cName: "Estland",
+        cID: "EE",
+        continent: "Europa"
+    },
+    {
+        cName: "Eswatini",
+        cID: "SZ",
+        continent: "Afrika"
+    },
+    {
+        cName: "Fidschi",
+        cID: "FJ",
+        continent: "Ozeanien"
+    },
+    {
+        cName: "Finnland",
+        cID: "FI",
+        continent: "Europa"
+    },
+    {
+        cName: "Frankreich",
+        cID: "FR",
+        continent: "Europa"
+    },
+    {
+        cName: "Gabun",
+        cID: "GA",
+        continent: "Afrika"
+    },
+    {
+        cName: "Gambia",
+        cID: "GM",
+        continent: "Afrika"
+    },
+    {
+        cName: "Georgien",
+        cID: "GE",
+        continent: "Asien"
+    },
+    {
+        cName: "Ghana",
+        cID: "GH",
+        continent: "Afrika"
+    },
+    {
+        cName: "Griechenland",
+        cID: "GR",
+        continent: "Europa"
+    },
+    {
+        cName: "Grönland",
+        cID: "GL",
+        continent: "Nordamerika"
+    },
+    {
+        cName: "Guatemala",
+        cID: "GT",
+        continent: "Nordamerika"
+    },
+    {
+        cName: "Guinea",
+        cID: "GN",
+        continent: "Afrika"
+    },
+    {
+        cName: "Guinea-Bissau",
+        cID: "GW",
+        continent: "Afrika"
+    },
+    {
+        cName: "Guyana",
+        cID: "GY",
+        continent: "Südamerika"
+    },
+    {
+        cName: "Haiti",
+        cID: "HT",
+        continent: "Nordamerika"
+    },
+    {
+        cName: "Honduras",
+        cID: "HN",
+        continent: "Nordamerika"
+    },
+    {
+        cName: "Indien",
+        cID: "IN",
+        continent: "Asien"
+    },
+    {
+        cName: "Indonesien",
+        cID: "ID",
+        continent: "Asien"
+    },
+    {
+        cName: "Irak",
+        cID: "IQ",
+        continent: "Asien"
+    },
+    {
+        cName: "Iran",
+        cID: "IR",
+        continent: "Asien"
+    },
+    {
+        cName: "Irland",
+        cID: "IE",
+        continent: "Europa"
+    },
+    {
+        cName: "Island",
+        cID: "IS",
+        continent: "Europa"
+    },
+    {
+        cName: "Israel",
+        cID: "IL",
+        continent: "Asien"
+    },
+    {
+        cName: "Italien",
+        cID: "IT",
+        continent: "Europa"
+    },
+    {
+        cName: "Jamaika",
+        cID: "JM",
+        continent: "Nordamerika"
+    },
+    {
+        cName: "Japan",
+        cID: "JP",
+        continent: "Asien"
+    },
+    {
+        cName: "Jemen",
+        cID: "YE",
+        continent: "Asien"
+    },
+    {
+        cName: "Jordanien",
+        cID: "JO",
+        continent: "Asien"
+    },
+    {
+        cName: "Kambodscha",
+        cID: "KH",
+        continent: "Asien"
+    },
+    {
+        cName: "Kamerun",
+        cID: "CM",
+        continent: "Afrika"
+    },
+    {
+        cName: "Kanada",
+        cID: "CA",
+        continent: "Nordamerika"
+    },
+    {
+        cName: "Kasachstan",
+        cID: "KZ",
+        continent: "Asien"
+    },
+    {
+        cName: "Katar",
+        cID: "QA",
+        continent: "Asien"
+    },
+    {
+        cName: "Kenia",
+        cID: "KE",
+        continent: "Afrika"
+    },
+    {
+        cName: "Kirgistan",
+        cID: "KG",
+        continent: "Asien"
+    },
+    {
+        cName: "Kolumbien",
+        cID: "CO",
+        continent: "Südamerika"
+    },
+    {
+        cName: "Demokratische Republik Kongo",
+        cID: "CD",
+        continent: "Afrika"
+    },
+    {
+        cName: "Republik Kongo",
+        cID: "CG",
+        continent: "Afrika"
+    },
+    {
+        cName: "Nordkorea",
+        cID: "KP",
+        continent: "Asien"
+    },
+    {
+        cName: "Südkorea",
+        cID: "KR",
+        continent: "Asien"
+    },
+    {
+        cName: "Kroatien",
+        cID: "HR",
+        continent: "Europa"
+    },
+    {
+        cName: "Kuba",
+        cID: "CU",
+        continent: "Nordamerika"
+    },
+    {
+        cName: "Kuwait",
+        cID: "KW",
+        continent: "Asien"
+    },
+    {
+        cName: "Laos",
+        cID: "LA",
+        continent: "Asien"
+    },
+    {
+        cName: "Lesotho",
+        cID: "LS",
+        continent: "Afrika"
+    },
+    {
+        cName: "Lettland",
+        cID: "LV",
+        continent: "Europa"
+    },
+    {
+        cName: "Libanon",
+        cID: "LB",
+        continent: "Asien"
+    },
+    {
+        cName: "Liberia",
+        cID: "LR",
+        continent: "Afrika"
+    },
+    {
+        cName: "Libyen",
+        cID: "LY",
+        continent: "Afrika"
+    },
+    {
+        cName: "Liechtenstein",
+        cID: "LI",
+        continent: "Europa"
+    },
+    {
+        cName: "Litauen",
+        cID: "LT",
+        continent: "Europa"
+    },
+    {
+        cName: "Luxemburg",
+        cID: "LU",
+        continent: "Europa"
+    },
+    {
+        cName: "Madagaskar",
+        cID: "MG",
+        continent: "Afrika"
+    },
+    {
+        cName: "Malawi",
+        cID: "MW",
+        continent: "Afrika"
+    },
+    {
+        cName: "Malaysia",
+        cID: "MY",
+        continent: "Asien"
+    },
+    {
+        cName: "Mali",
+        cID: "ML",
+        continent: "Afrika"
+    },
+    {
+        cName: "Malta",
+        cID: "MT",
+        continent: "Europa"
+    },
+    {
+        cName: "Marokko",
+        cID: "MA",
+        continent: "Afrika"
+    },
+    {
+        cName: "Mauretanien",
+        cID: "MR",
+        continent: "Afrika"
+    },
+    {
+        cName: "Mexiko",
+        cID: "MX",
+        continent: "Nordamerika"
+    },
+    {
+        cName: "Moldau",
+        cID: "MD",
+        continent: "Europa"
+    },
+    {
+        cName: "Mongolei",
+        cID: "MN",
+        continent: "Asien"
+    },
+    {
+        cName: "Montenegro",
+        cID: "ME",
+        continent: "Europa"
+    },
+    {
+        cName: "Mosambik",
+        cID: "MZ",
+        continent: "Afrika"
+    },
+    {
+        cName: "Namibia",
+        cID: "NA",
+        continent: "Afrika"
+    },
+    {
+        cName: "Nepal",
+        cID: "NP",
+        continent: "Asien"
+    },
+    {
+        cName: "Neukaledonien",
+        cID: "NC",
+        continent: "Ozeanien"
+    },
+    {
+        cName: "Neuseeland",
+        cID: "NZ",
+        continent: "Ozeanien"
+    },
+    {
+        cName: "Nicaragua",
+        cID: "NI",
+        continent: "Nordamerika"
+    },
+    {
+        cName: "Niederlande",
+        cID: "NL",
+        continent: "Europa"
+    },
+    {
+        cName: "Niger",
+        cID: "NE",
+        continent: "Afrika"
+    },
+    {
+        cName: "Nigeria",
+        cID: "NG",
+        continent: "Afrika"
+    },
+    {
+        cName: "Nordmazedonien",
+        cID: "MK",
+        continent: "Europa"
+    },
+    {
+        cName: "Norwegen",
+        cID: "NO",
+        continent: "Europa"
+    },
+    {
+        cName: "Oman",
+        cID: "OM",
+        continent: "Asien"
+    },
+    {
+        cName: "Österreich",
+        cID: "AT",
+        continent: "Europa"
+    },
+    {
+        cName: "Timor-Leste",
+        cID: "TL",
+        continent: "Asien"
+    },
+    {
+        cName: "Pakistan",
+        cID: "PK",
+        continent: "Asien"
+    },
+    {
+        cName: "Palästina",
+        cID: "PS",
+        continent: "Asien"
+    },
+    {
+        cName: "Panama",
+        cID: "PA",
+        continent: "Nordamerika"
+    },
+    {
+        cName: "Papua-Neuguinea",
+        cID: "PG",
+        continent: "Ozeanien"
+    },
+    {
+        cName: "Paraguay",
+        cID: "PY",
+        continent: "Südamerika"
+    },
+    {
+        cName: "Peru",
+        cID: "PE",
+        continent: "Südamerika"
+    },
+    {
+        cName: "Philippinen",
+        cID: "PH",
+        continent: "Asien"
+    },
+    {
+        cName: "Polen",
+        cID: "PL",
+        continent: "Europa"
+    },
+    {
+        cName: "Portugal",
+        cID: "PT",
+        continent: "Europa"
+    },
+    {
+        cName: "Puerto Rico",
+        cID: "PR",
+        continent: "Nordamerika"
+    },
+    {
+        cName: "Ruanda",
+        cID: "RW",
+        continent: "Afrika"
+    },
+    {
+        cName: "Rumänien",
+        cID: "RO",
+        continent: "Europa"
+    },
+    {
+        cName: "Russland",
+        cID: "RU",
+        continent: "Europa"
+    },
+    {
+        cName: "Sambia",
+        cID: "ZM",
+        continent: "Afrika"
+    },
+    {
+        cName: "Saudi-Arabien",
+        cID: "SA",
+        continent: "Asien"
+    },
+    {
+        cName: "Schweden",
+        cID: "SE",
+        continent: "Europa"
+    },
+    {
+        cName: "Schweiz",
+        cID: "CH",
+        continent: "Europa"
+    },
+    {
+        cName: "Senegal",
+        cID: "SN",
+        continent: "Afrika"
+    },
+    {
+        cName: "Serbien",
+        cID: "RS",
+        continent: "Europa"
+    },
+    {
+        cName: "Sierra Leone",
+        cID: "SL",
+        continent: "Afrika"
+    },
+    {
+        cName: "Simbabwe",
+        cID: "ZW",
+        continent: "Afrika"
+    },
+    {
+        cName: "Singapur",
+        cID: "SG",
+        continent: "Asien"
+    },
+    {
+        cName: "Slowakei",
+        cID: "SK",
+        continent: "Europa"
+    },
+    {
+        cName: "Slowenien",
+        cID: "SL",
+        continent: "Europa"
+    },
+    {
+        cName: "Somalia",
+        cID: "SO",
+        continent: "Afrika"
+    },
+    {
+        cName: "Spanien",
+        cID: "ES",
+        continent: "Europa"
+    },
+    {
+        cName: "Sri Lanka",
+        cID: "LK",
+        continent: "Afrika"
+    },
+    {
+        cName: "Südafrika",
+        cID: "ZA",
+        continent: "Afrika"
+    },
+    {
+        cName: "Sudan",
+        cID: "SD",
+        continent: "Afrika"
+    },
+    {
+        cName: "Südsudan",
+        cID: "SS",
+        continent: "Afrika"
+    },
+    {
+        cName: "Suriname",
+        cID: "SR",
+        continent: "Südamerika"
+    },
+    {
+        cName: "Syrien",
+        cID: "SY",
+        continent: "Asien"
+    },
+    {
+        cName: "Tadschikistan",
+        cID: "TJ",
+        continent: "Asien"
+    },
+    {
+        cName: "Taiwan",
+        cID: "TW",
+        continent: "Asien"
+    },
+    {
+        cName: "Tansania",
+        cID: "TZ",
+        continent: "Afrika"
+    },
+    {
+        cName: "Thailand",
+        cID: "TH",
+        continent: "Asien"
+    },
+    {
+        cName: "Togo",
+        cID: "TG",
+        continent: "Afrika"
+    },
+    {
+        cName: "Tschad",
+        cID: "TD",
+        continent: "Afrika"
+    },
+    {
+        cName: "Tschechien",
+        cID: "CZ",
+        continent: "Europa"
+    },
+    {
+        cName: "Tunesien",
+        cID: "TN",
+        continent: "Afrika"
+    },
+    {
+        cName: "Türkei",
+        cID: "TR",
+        continent: "Asien"
+    },
+    {
+        cName: "Turkmenistan",
+        cID: "TM",
+        continent: "Asien"
+    },
+    {
+        cName: "Uganda",
+        cID: "UG",
+        continent: "Afrika"
+    },
+    {
+        cName: "Ukraine",
+        cID: "UA",
+        continent: "Europa"
+    },
+    {
+        cName: "Ungarn",
+        cID: "HU",
+        continent: "Europa"
+    },
+    {
+        cName: "Uruguay",
+        cID: "UY",
+        continent: "Südamerika"
+    },
+    {
+        cName: "Usbekistan",
+        cID: "UZ",
+        continent: "Asien"
+    },
+    {
+        cName: "Venezuela",
+        cID: "VE",
+        continent: "Südamerika"
+    },
+    {
+        cName: "Vereinigte Arabische Emirate",
+        cID: "AE",
+        continent: "Asien"
+    },
+    {
+        cName: "Vereinigte Staaten",
+        cID: "US",
+        continent: "Nordamerika"
+    },
+    {
+        cName: "Vereinigtes Königreich",
+        cID: "GB",
+        continent: "Europa"
+    },
+    {
+        cName: "Vietnam",
+        cID: "VN",
+        continent: "Asien"
+    },
+    {
+        cName: "Westsahara",
+        cID: "EH",
+        continent: "Afrika"
+    },
+    {
+        cName: "Zentralafrikanische Republik",
+        cID: "CF",
+        continent: "Afrika"
+    },
+    {
+        cName: "Zypern",
+        cID: "CY",
+        continent: "Europa"
+    },
+]
--- project/scripts/mapScript.js
+++ project/scripts/mapScript.js
@@ -1,3 +1,5 @@
+/// <reference path="../data/data.js" />
+/// <reference path="../data/data2.js" />
 var map = new jsVectorMap({
     map: 'world',
     selector: '#map', 
@@ -5,10 +7,57 @@ var map = new jsVectorMap({
     regionsSelectableOne: true
 })
 let timer;
+let country = null
+let mapData = document.getElementById('mapData')
+let sum = 0
+let continentArray = ["Europa", "Asien", "Afrika", "Ozeanien", "Nordamerika", "Südamerika"]
 loop()
 function loop() {
     if(map.getSelectedRegions() != "") {
-        console.log(map.getSelectedRegions())
+        country = map.getSelectedRegions()
+        for (let i = 0; i < data2.length; i++) {
+            for (let j = 0; j < continentArray.length; j++) {
+                if(data2[i].continent == continentArray[j]){
+                    setCountry(continentArray[j])
+                }
+            }
+        }
+        window.scrollTo({
+            top: 100,
+            left: 100,
+            behavior: "smooth",
+          });
     }
+    country = null
+    map.clearSelectedRegions();
     timer = setTimeout(loop, 500);
+}
+function setCountry(continent){
+    if(country == null){
+        return
+    }
+    let Baustein = ""
+    for (let i = 0; i < window.data.length; i++) {
+        for (let j = 0; j < window.data[i].Countries.length; j++) {
+            if(window.data[i].Countries[j].CountryCode == country){
+                for (let x = 0; x < window.data[i].Countries[j].Verkaufszahlen.length; x++) {
+                    sum += window.data[i].Countries[j].Verkaufszahlen[x]
+                }
+                for (let x = 0; x < 5; x++) {
+                    let y = 0
+                    for (y = 0; y < data2.length; y++) {
+                        if(data2[y].cID == country){
+                            break
+                        }
+                    }
+                    Baustein += `<div id="displayItems" onclick="expandNumbers()"><img id="displayItemsImage" src="./images/arrow.png"><h3>${data2[y].cName}</h3></div>`
+                }
+                cont = document.getElementById(continent)
+                cont.innerHTML += Baustein
+            }
+        }
+    }
+}
+function expand(continent) {
+    setCountry(continent)
 }
